e7feea23aba164cf290256170b588b4980eeb00f
Milos <milospejic2001@gmail.com>
2022-09-23 16:57:20 +0200

    Added HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import Carousel from "react-bootstrap/Carousel";
+import { Link } from "react-router-dom";
 
 export default function HomeScreen() {
   return (
@@ -7,6 +9,53 @@ export default function HomeScreen() {
       <Helmet>
         <title>gymPro</title>
       </Helmet>{" "}
+      <Carousel>
+        <Carousel.Item>
+          <Link to="/aboutus">
+            <img
+              className="d-block w-100"
+              src="images/p1.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>About us</h3>
+              <p>
+                Let us tell you the story of our beggining and our plans for the
+                future
+              </p>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <Link to="/membershipdeals">
+            <img
+              className="d-block w-100"
+              src="images/p2.jpg"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>What do we offer?</h3>
+              <p>Explore our special membership plans</p>
+            </Carousel.Caption>{" "}
+          </Link>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          {" "}
+          <Link to="/contactus">
+            <img
+              className="d-block w-100"
+              src="images/p3.jpg"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Want to know more?</h3>
+              <p>If you want to know more we are at your service</p>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
